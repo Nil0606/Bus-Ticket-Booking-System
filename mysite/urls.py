25bdde8a66bbf5863180bca9a491be7ba6be3b09
Nil patel <nilpatel0606@gmail.com>
2022-05-12 13:45:55 +0530

    changed url file

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,5 +5,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
-]+static(settings.MEDIA_URL,
+]
+if settings.DEBUG:
+    urlpatterns+static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
